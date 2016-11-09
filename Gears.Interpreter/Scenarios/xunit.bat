9be76f2e3fd2bbe5246311e32cb758af4bcaf9a3
Petr Hovorka <petr.hovorka@simplity.eu>
2016-11-09 15:59:19 +0100

    #5342 - xunit.bat added comment about junit-viewer installation

--- Gears.Interpreter/Scenarios/xunit.bat
+++ Gears.Interpreter/Scenarios/xunit.bat
@@ -1,2 +1,4 @@
+rem this script requires node and junit-viewer to be installed.
+rem to install junit-viewer, type node install -g junit-viewer
 ..\Gears.Interpreter.exe Test1.xlsx -junitxml
 junit-viewer.cmd --results=\ --save=report.html
